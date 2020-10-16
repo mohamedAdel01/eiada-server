49ab6b28253c1c3fe213c023569557a0439434b4
mohamedadel01 <mohamed7adel96@gmail.com>
2020-10-16 15:49:23 +0200

    edit

--- src/models/branches.js
+++ src/models/branches.js
@@ -2,16 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Clinic = new Schema({
-    name: {
+    address: {
         type: String,
         required: true
     },
-    owner_id: {
-        type: String,
-        required: true
-    },
-    logo: String,
-    theme_settings: String
+    specializations_ids: [String]
 })
 
 module.exports = mongoose.model('Clinic', Clinic)
