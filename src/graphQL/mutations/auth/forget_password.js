7cfcb953df8af663723bfa3883344aa37895ffbf
mohamedadel01 <mohamed7adel96@gmail.com>
2020-10-30 19:18:46 +0200

    edit

--- src/graphQL/mutations/auth/forget_password.js
+++ src/graphQL/mutations/auth/forget_password.js
@@ -1,19 +1,14 @@
-const bcrypt = require("bcrypt");
-const ObjectId = require("mongodb").ObjectID;
 const graphql = require("graphql");
 const { GraphQLString } = graphql;
+const { MessageType } = require("../../types/types");
+const { validate } = require("../../../validations");
+const { checkUserExistance, decodeToken } = require("../../../policies");
+const { send_verification_email } = require("../../../controllers/emails");
+
+const bcrypt = require("bcrypt");
+const ObjectId = require("mongodb").ObjectID;
 const Email_Verification = require("../../../models/email_verify");
 const User = require("../../../models/user");
-const {
-  send_verification_email,
-} = require("../../../controllers/auth/validate_controller");
-
-// GRAPHQL TYPES
-const { MessageType } = require("../../types/types");
-const { decodeToken } = require("../../../policies");
-const {
-  forgetPasswordRequest,
-} = require("../../../controllers/auth/forget_password_controller");
 
 const forgetPasswordRequestMutation = {
   type: MessageType,
@@ -21,58 +16,64 @@ const forgetPasswordRequestMutation = {
     email: { type: GraphQLString },
   },
 
-  async resolve(parent, args, root) {
-    return await forgetPasswordRequest(args.email);
+  async resolve(parent, args) {
+    let v_errors = validate(args);
+    if (v_errors.length) return { errors: v_errors };
+
+    let { exUser, p_userErrors } = await checkUserExistance(args.email, true);
+    if (p_userErrors.length) return { errors: p_userErrors };
+
+    return await send_verification_email(exUser, "password");
   },
 };
 
-const changePasswordMutation = {
-  type: MessageType,
-  args: {
-    verification_code: { type: GraphQLString },
-    new_password: { type: GraphQLString },
-  },
+// const changePasswordMutation = {
+//   type: MessageType,
+//   args: {
+//     verification_code: { type: GraphQLString },
+//     new_password: { type: GraphQLString },
+//   },
 
-  async resolve(_, args) {
-    let all_errors = [];
+//   async resolve(_, args) {
+//     let all_errors = [];
 
-    let { errors, user } = decodeToken(args.verification_code, true);
-    if (errors.length) return { errors };
+//     let { errors, user } = decodeToken(args.verification_code, true);
+//     if (errors.length) return { errors };
 
-    let exVerification = await Email_Verification.findOne({
-      user_id: user.user_id,
-    });
+//     let exVerification = await Email_Verification.findOne({
+//       user_id: user.user_id,
+//     });
 
-    if (!exVerification || exVerification.code != user.code) {
-      all_errors.push({
-        key: "Validation",
-        message: "Expired code, We will resend you another one",
-      });
+//     if (!exVerification || exVerification.code != user.code) {
+//       all_errors.push({
+//         key: "Validation",
+//         message: "Expired code, We will resend you another one",
+//       });
 
-      let exUser = await User.findById(user.user_id);
-      await send_verification_email(exUser, "password");
+//       let exUser = await User.findById(user.user_id);
+//       await send_verification_email(exUser, "password");
 
-      return {
-        errors: all_errors,
-      };
-    }
+//       return {
+//         errors: all_errors,
+//       };
+//     }
 
-    const new_password = bcrypt.hashSync(args.new_password, 10);
+//     const new_password = bcrypt.hashSync(args.new_password, 10);
 
-    await User.findOneAndUpdate(
-      { _id: ObjectId(user.user_id) },
-      { password: new_password }
-    );
-    await Email_Verification.findOneAndDelete({ user_id: user.user_id });
+//     await User.findOneAndUpdate(
+//       { _id: ObjectId(user.user_id) },
+//       { password: new_password }
+//     );
+//     await Email_Verification.findOneAndDelete({ user_id: user.user_id });
 
-    return {
-      message: "Password changed successfully successfully",
-      errors: [],
-    };
-  },
-};
+//     return {
+//       message: "Password changed successfully successfully",
+//       errors: [],
+//     };
+//   },
+// };
 
 module.exports = {
   forget_password_request: forgetPasswordRequestMutation,
-  change_password: changePasswordMutation,
+  // change_password: changePasswordMutation,
 };
--- src/graphQL/mutations/index.js
+++ src/graphQL/mutations/index.js
@@ -2,17 +2,17 @@ const Register = require("./auth/register");
 const { Verify_Email, resend_verification_email } = require("./auth/verify_email");
 const Login = require("./auth/login");
 // const Create_Clinic = require("./create/Create_Clinic");
-// const {
-//   forget_password_request,
-//   change_password,
-// } = require("./auth/forget_password");
+const {
+  forget_password_request,
+  // change_password,
+} = require("./auth/forget_password");
 
 module.exports = {
   Register,
   Verify_Email,
   resend_verification_email,
   Login,
-  // forget_password_request,
+  forget_password_request,
   // change_password,
   // Create_Clinic,
 };
