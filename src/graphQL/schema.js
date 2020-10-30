db62682f52367f491404d2e532321ddcfcddfb2f
mohamedadel01 <mohamed7adel96@gmail.com>
2020-10-30 16:27:59 +0200

    e

--- src/graphQL/schema.js
+++ src/graphQL/schema.js
@@ -1,13 +1,10 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLSchema } = graphql;
 
-// GRAPHQL QUERIES
 const Queries = require("./queries");
 
-// MUTATIONS
 const Mutations = require("./mutations");
 
-// GRAPHQL ROOT QUERY
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
