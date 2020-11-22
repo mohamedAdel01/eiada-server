cfb1551035a08243f82dbe38b53235728bab80da
mohamedadel01 <mohamed7adel96@gmail.com>
2020-11-22 02:05:19 +0200

    make it better

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`, {
+// mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`, {
+mongoose.connect(`mongodb://eiada29:eiada29@ds145223.mlab.com:45223/eiada`, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
