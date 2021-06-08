96293f743a90c42d8509b8e9bbf0316446b21569
mohamedadel01 <mohamed7adel96@gmail.com>
2021-06-08 19:34:20 +0200

    edit usre

--- src/graphQL/queries/user.js
+++ src/graphQL/queries/user.js
@@ -1,5 +1,5 @@
 const graphql = require("graphql");
-const { GraphQLID, GraphQLList } = graphql;
+const { GraphQLID, GraphQLList, GraphQLInt, GraphQLObjectType } = graphql;
 
 // GRAPHQL TYPES
 const { UserType } = require("../types/types");
@@ -7,13 +7,32 @@ const { UserType } = require("../types/types");
 // MONGODB MODELS
 const userModel = require("../../models/user");
 
-const UserQueries = {
+const USERS = {
   Users: {
-    type: new GraphQLList(UserType),
-    resolve() {
-      return userModel.find({});
+    type: new GraphQLObjectType({
+      name: "users",
+      fields: () => ({
+        users: {type: new GraphQLList(UserType)},
+        total: {type: GraphQLInt},
+      }),
+    }),
+
+    args: {
+      page: { type: GraphQLInt },
+      limit: { type: GraphQLInt },
+    },
+    resolve(parent, args) {
+      let page = args.page ? args.page : 1;
+      let limit = args.limit ? args.limit : 10;
+      return {
+        users: userModel
+          .find({})
+          .limit(parseInt(limit))
+          .skip(parseInt(limit * (page - 1))),
+        total: userModel.find().countDocuments(),
+      };
     },
   },
 };
 
-module.exports = UserQueries;
+module.exports = USERS;
